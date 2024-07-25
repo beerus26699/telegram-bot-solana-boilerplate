8f3d18bdc029d564a439faa8293d07c279b61820
HaiNV266 <62513421+beerus26699@users.noreply.github.com>
2024-07-25 14:35:54 +0700

    .

--- src/modules/wallets/wallets.service.ts
+++ src/modules/wallets/wallets.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/user.dto';
+import { CreateUserDto } from './dto/wallet.dto';
 import * as bcrypt from 'bcryptjs';
 import { Wallet } from 'src/schemas/wallet.schema';
 
